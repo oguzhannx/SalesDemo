eb5894018504b2ccab529e6b7e9b0a104a15a3dc
oguzhannx <oguzhanoztrk00@gmail.com>
2024-05-05 23:32:57 +0300

    asd

---
+++ SalesDemo.Api/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesDemo.Api.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SalesDemo.Business/Concrete/CompanyService.cs
@@ -0,0 +1,13 @@
+﻿using SalesDemo.Business.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesDemo.Business.Concrete
+{
+    public class CompanyService : ICompanyService
+    {
+    }
+}
